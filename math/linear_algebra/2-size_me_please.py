44000dd949640aab72b0632ab48ac7328dc7d14b
Thomas Messerer <tc.messerer@gmail.com>
2026-06-04 16:36:49 +0000

    2: adjusted function

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 """Module for linear algebra"""
-import numpy as np
+
 
 def matrix_shape(matrix):
     """returns the shape of a matrix.
-    args: matrix 
+    args: matrix
     """
+    this_row = matrix
+    shape = []
 
-mat1 = [[1, 2], [3, 4]]
-print(matrix_shape(mat1))
-mat2 = [[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]],
-        [[16, 17, 18, 19, 20], [21, 22, 23, 24, 25], [26, 27, 28, 29, 30]]]
-
-print(len(mat1))
-array1 = np.ndarray(mat1)
-print(array1.shape())
+    while isinstance(this_row, list):
+        if not this_row:
+            return [0]
+            break
+        shape.append(len(this_row))
+        this_row = this_row[0]
+    return shape
