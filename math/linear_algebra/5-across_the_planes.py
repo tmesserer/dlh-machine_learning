7178fe3577e083380a84a7f60241987421a33689
Thomas Messerer <tc.messer@gmail.com>
2026-06-06 16:41:15 +0000

    5: added file across_the_planes

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""Module for linear algebra"""
+
+
+def matrix_shape(matrix):
+    shape = []
+    while isinstance(matrix, list):
+        if not matrix:
+            return None
+        shape.append(len(matrix))
+        matrix = matrix[0]
+    return shape
+
+
+def add_matrices2D(mat1, mat2):
+    """checks if matrices are same shape.
+    If they are, returns an element-wise addition."""
+    if matrix_shape(mat1) != matrix_shape(mat2):
+        return None
+    new_matrix = []
+    new_row = []
+    for i in range(0, len(mat1)):
+        # print(f"i = {i}")
+        for j in range(0, len(mat1[i])):
+            # print(f"j = {j}")
+            new_row.append(mat1[i][j] + mat2[i][j])
+            # print(f"new row: {new_row}")
+        new_matrix.append(new_row)
+        new_row = []
+    return new_matrix
