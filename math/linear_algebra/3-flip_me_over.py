a1440fff39a120b07673f4dda30fce753217ed71
Thomas Messerer <tc.messer@gmail.com>
2026-06-06 15:31:11 +0000

    3: adjusted logic

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -3,38 +3,18 @@
 
 
 def matrix_transpose(matrix):
-    """resturns the shape of a matrix.
+    """returns the transposed matrix of a matrix.
     args: matrix
     """
     new_matrix = []
-    this_row = matrix
-    #while isinstance(this_row[0], list):
-        #this_row = this_row[0]
-    #if not this_row:
-        #return [0]
-        #break
-    new_matrix = []
-    for i in range(0, len(this_row[0])):
-        for j in range(0, len(this_row)):
-            new_row = []
-            new_row.append(this_row[j][i])
-            print(new_row)
+    new_row = []
+    # while isinstance(matrix[0], list):
+    # matrix = matrix[0]
+    if not matrix:
+        return []
+    for i in range(0, len(matrix[0])):
+        for j in range(0, len(matrix)):
+            new_row.append(matrix[j][i])
         new_matrix.append(new_row)
-        print(new_matrix)
-
-mat1 = [[1, 2], [3, 4]]
-mat2 = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15],
-        [16, 17, 18, 19, 20], [21, 22, 23, 24, 25], [26, 27, 28, 29, 30]]
-
-matrix_transpose(mat1)
-
-"""
-for i in this_row:
-    print(f"i: {i}, value: {this_row[i]}")
-    for j in range(0, len(this_row)):
-        print(f"j: {j}, value: {this_row[i]}")
-        new_matrix.append(this_row[j])
-shape.append(len(this_row))
-return new_matrix
-"""
-
+        new_row = []
+    return new_matrix
