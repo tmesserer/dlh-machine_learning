29f62090f5fc863c7e58468af94d6ef66fc357bf
Your Name <you@example.com>
2026-06-09 18:04:17 +0000

    12: first try

--- math/linear_algebra/12-bracing_the_elements.py
+++ math/linear_algebra/12-bracing_the_elements.py
@@ -2,6 +2,16 @@
 """Module for linear algebra"""
 
 
-def np_transpose(matrix):
+def np_elementwise(mat1, mat2):
     """Returns the shape of a NumPy array."""
-    return matrix.T
+    return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
+
+
+"""
+mat1 = np.array([[11, 22, 33], [44, 55, 66]])
+mat2 = np.array([[1, 2, 3], [4, 5, 6]])
+np_elementwise(mat1, mat2)
+print(mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
+print(mat1)
+print(mat2)
+"""
