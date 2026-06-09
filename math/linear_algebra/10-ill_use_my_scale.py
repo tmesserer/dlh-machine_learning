f2d6fe36e305eb031b2d75ccefbb8f7f3ecf3f1f
Your Name <you@example.com>
2026-06-09 17:22:28 +0000

    10: adjusted with .shape

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -1,22 +1,7 @@
 #!/usr/bin/env python3
 """Module for linear algebra"""
-import numpy as np
 
 
 def np_shape(matrix):
     """Returns the shape of a NumPy array."""
     return matrix.shape
-
-
-"""
-def np_shape(matrix):
-    gives the shape of a matrix.
-    Args: matrix
-    if isinstance(mat_shape, list):
-        pass
-    else:
-        mat_shape = []
-    mat_shape.append(len(matrix))
-    if matrix.ndim > 1:
-        return (len(matrix) + np_shape(matrix[0]))
-"""
