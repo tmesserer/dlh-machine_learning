8a6bc12d43c3c5862a9fb92f344af006ed1f6c88
Thomas Messerer <tc.messerer>
2026-06-11 19:46:11 +0000

    102: started file

--- math/linear_algebra/102-squashed_like_sardines.py
+++ math/linear_algebra/102-squashed_like_sardines.py
@@ -2,18 +2,44 @@
 """Module for linear algebra"""
 
 
-def add_matrices(mat1, mat2):
-    """Element-wise addition of two matrices of equal shape."""
-
-    # both are lists --> recurse
-    if isinstance(mat1, list) and isinstance(mat2, list):
-        if len(mat1) != len(mat2):
-            return None
-        return [add_matrices(a, b) for a, b in zip(mat1, mat2)]
-
-    # structure mismatch
-    if isinstance(mat1, list) or isinstance(mat2, list):
+def cat_matrices(mat1, mat2, axis=0):
+    """Matrix concatenation alongside a specified axis"""
+    new_matrix = []
+    #first part: drill down to right level
+for i in range(0, axis):
+    try:
+        mat1[0]
+        mat1 = mat1[0]
+    except:
         return None
 
-    # base case (scalars)
-    return mat1 + mat2
+    #when at right dimension (still list, no number) --> concatenate:
+    if isinstance(mat1, list) and isinstance(mat2, list):
+        for i in range(len(mat1)):
+            new_matrix.append(mat1 + mat2)
+    else:
+        return None
+
+
+
+
+import numpy as np
+import time
+cat_matrices = __import__('102-squashed_like_sardines').cat_matrices
+
+mat1 = [1, 2, 3]
+mat2 = [4, 5, 6]
+np_mat1 = np.array(mat1)
+np_mat2 = np.array(mat2)
+
+t0 = time.time()
+m = cat_matrices(mat1, mat2)
+t1 = time.time()
+print(t1 - t0)
+print(m)
+t0 = time.time()
+np.concatenate((np_mat1, np_mat2))
+t1 = time.time()
+print(t1 - t0, "\n")
+
+print(mat1 + mat2)
