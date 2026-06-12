9eb1e0e3af04996890cb8d541fc0d60af509a98d
Thomas Messerer <tc.messerer-gmail.com>
2026-06-12 17:11:39 +0000

    102: massive change for edge cases

--- math/linear_algebra/102-squashed_like_sardines.py
+++ math/linear_algebra/102-squashed_like_sardines.py
@@ -2,8 +2,63 @@
 """Module for linear algebra"""
 
 
+def matrix_shape(matrix):
+    """returns the shape of a matrix.
+    args: matrix
+    """
+    shape = []
+    this_row = matrix
+
+    while isinstance(this_row, list):
+        if not this_row:
+            return [0]
+        shape.append(len(this_row))
+        this_row = this_row[0]
+    return shape
+
+
 def cat_matrices(mat1, mat2, axis=0):
     """Matrix concatenation alongside a specified axis"""
+    shape1 = matrix_shape(mat1)
+    shape2 = matrix_shape(mat2)
+
+    if axis >= len(shape1) or axis >= len(shape2):
+        return None
+
+    # remove the element at index `axis` from each shape,
+    # then compare the rest
+    if (shape1[:axis] + shape1[axis + 1:]
+            != shape2[:axis] + shape2[axis + 1:]):
+        return None
+
+    if axis > 0:
+        return [cat_matrices(m1, m2, axis - 1) for m1, m2 in zip(mat1, mat2)]
+
+    return mat1 + mat2
+
+
+"""
+def matrix_shape(matrix):
+    """
+# returns the shape of a matrix.
+# args: matrix
+"""
+    this_row = matrix
+    shape = []
+
+    while isinstance(this_row, list):
+        if not this_row:
+            return [0]
+            break
+        shape.append(len(this_row))
+        this_row = this_row[0]
+    return shape
+
+
+def cat_matrices(mat1, mat2, axis=0):
+    # Matrix concatenation alongside a specified axis
+    """
+"""
     if axis == 0 and (
         isinstance(mat1[0], list) != isinstance(mat2[0], list)
         or (isinstance(mat1[0], list) and len(mat1[0]) != len(mat2[0]))
@@ -18,7 +73,8 @@ def cat_matrices(mat1, mat2, axis=0):
     return mat1 + mat2
 
 
-"""
+
+
 mat1 = [1, 2, 3]
 mat2 = [4, 5, 6, 7]
 new_matrix = mat1 + mat2
@@ -49,4 +105,11 @@ mat5 = [[[[11, 12, 13, 14], [15, 16, 17, 18]],
 cat_matrices(mat3, mat4, axis=3)
 cat_matrices(mat3, mat5, axis=1)
 
+matrix_shape(mat3)
+matrix_shape(mat4)
+matrix_shape(mat5)
+
+matrix_shape(mat3)
+my_shape = [2, 3, 2 ,4]
+print(my_shape[0])
 """
