d484724f5acce5e80181d19e66d4cd40dfe0c66b
Thomas <tc.messerer@gmail.com>
2026-06-14 21:37:49 +0200

    11, 13: adjusted details

--- pipeline/databases/11-rating_shows.sql
+++ pipeline/databases/11-rating_shows.sql
@@ -1,5 +1,5 @@
 -- this comment is a description of the script below
-SELECT a.title, SUM(b.rate) AS rating_sum
+SELECT a.title, SUM(b.rate) AS rating
 FROM tv_shows a
     INNER JOIN tv_show_ratings b
     ON a.id = b.show_id
--- pipeline/databases/13-uniq_users.sql
+++ pipeline/databases/13-uniq_users.sql
@@ -1,6 +1,6 @@
 -- this comment is a description of the script below
 CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY
-    email STR(255Char) never null UNIQUE
-    name STR(255Char)
-)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+);
