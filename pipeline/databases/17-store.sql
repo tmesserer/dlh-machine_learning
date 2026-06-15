f86a0e8da3429114aed6a6895a8309c25d7734cb
Thomas <tc.messerer@gmail.com>
2026-06-15 16:25:41 +0200

    17: added DELIMITER

--- pipeline/databases/17-store.sql
+++ pipeline/databases/17-store.sql
@@ -1,6 +1,8 @@
 -- this comment is a description of the script below
+DELIMITER $$
 CREATE TRIGGER dec_stock
 AFTER INSERT ON orders FOR EACH ROW
 BEGIN
     UPDATE items SET quantity = quantity - NEW.number WHERE NEW.item_name = name;
 END;
+DELIMITER ;
