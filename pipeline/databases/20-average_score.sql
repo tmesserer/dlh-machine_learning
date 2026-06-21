fb88f12b2334391688d669d5091f041c390b1e46
Thomas <tc.messerer@gmail.com>
2026-06-21 21:16:34 +0200

    20: added file

--- pipeline/databases/20-average_score.sql
+++ pipeline/databases/20-average_score.sql
@@ -1,14 +1,11 @@
 -- this comment is a description of the script below
 DELIMITER $$
-CREATE PROCEDURE AddBonus
-    (IN user_id VARCHAR(255), IN project_name VARCHAR(255), IN score FLOAT)
+CREATE PROCEDURE ComputeAverageScoreForUser
+    (IN user_id VARCHAR(255))
 BEGIN
-    DECLARE project_id INT;
-    IF NOT EXISTS (SELECT name FROM projects WHERE name = project_name) THEN
-        INSERT INTO projects (id, name) VALUES (DEFAULT, project_name);
-    END IF;
-    SELECT id INTO project_id FROM projects WHERE name = project_name;
-    INSERT INTO corrections (user_id, project_id, score) 
-        VALUES(user_id, project_id, score);
+    DECLARE avg_score FLOAT;
+    SELECT AVG(score) INTO avg_score FROM corrections a WHERE a.user_id = user_id;
+    UPDATE users b SET b.average_score = avg_score WHERE id = user_id;
 END$$
 DELIMITER ;
+
