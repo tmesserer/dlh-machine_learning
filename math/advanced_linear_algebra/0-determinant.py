478acf4208116be852878ad1dc7e35b81fa67cfd
Thomas <tc.messerer@gmail.com>
2026-06-21 14:57:11 +0200

    0: added file

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -1,60 +1,69 @@
 #!/usr/bin/env python3
 """Module for advanced linear algebra"""
-    
+
+
+def matrix_multiplication(mult_factor, matrix):
+    """multiplies matrix by a constant factor"""
+    new_matrix = []
+    for i in range(0, len(matrix)):
+        if not isinstance(matrix[i], list):
+            new_matrix.append(matrix[i] * mult_factor)
+        else:
+            result = matrix_multiplication(mult_factor, matrix[i])
+            new_matrix.append(result)
+    return new_matrix
+
+
+def submatrix_calc(matrix, c):
+    """calculates the minor matrix at element c
+    Note: c refers to python length, not element of determinant
+    """
+    new_matrix = []
+    new_row = []
+    for i in range(1, len(matrix)):
+        new_row.extend(matrix[i][:c])
+        new_row.extend(matrix[i][c+1:])
+        new_matrix.append(new_row)
+        new_row = []
+    return new_matrix
+
+
+def add_matrices(mat1, mat2):
+    """checks if matrices are same length.
+    If they are, returns an element-wise addition."""
+    if len(mat1) != len(mat2):
+        return None
+    sum_arr = []
+    if not isinstance(mat1[0], list):
+        for i in range(0, len(mat1)):
+            sum_arr.append(mat1[i] + mat2[i])
+    else:
+        for i in range(0, len(mat1)):
+            result = add_matrices(mat1[i], mat2[i])
+            sum_arr.append(result)
+    return sum_arr
+
 
 def determinant(matrix):
     """calculates the determinant of a matrix (list of lists)"""
-    try:
-        if matrix == [[]]:
-            print("empty Matrix")
-            return 1
-            
-        elif len(matrix[0]) == 1 and isinstance(matrix[0][0], int):
-            print("1x1 Matrix")
-            return matrix[0]
+    if not isinstance(matrix, list) or len(matrix) == 0 or \
+            not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a list of lists")
+    if matrix == [[]]:
+        return 1
 
-        elif len(matrix[0]) == 2 and isinstance(matrix[0][0], int) and isinstance(matrix[0][1], int):
-            print("2x2 Matrix")
-            return [(matrix[0][0]*matrix[1][1]) - (matrix[0][1]*matrix[1][0])]
+    if not all(len(row) == len(matrix) for row in matrix):
+        raise ValueError("matrix must be a square matrix")
 
-        #3x3 and beyond    
-        else:
-            print("pass")
-            return len(matrix[0]) * #add rest
-        #determinant(cofactor matrix1)
-        
-    #Exceptions    
-    except TypeError: #not a list of lists
-        return "matrix must be a list of lists"
-    except ValueError: #not a square matrix
+    if len(matrix[0]) == 1:
+        return matrix[0][0]
+    elif len(matrix[0]) == 2:
+        return (matrix[0][0]*matrix[1][1]) - (matrix[0][1]*matrix[1][0])
+    else:
+        result = 0
+        for i in range(len(matrix[0])):
+            minor = submatrix_calc(matrix, i)
+            sign = (-1) ** i
+            result += sign * matrix[0][i] * determinant(minor)
+        return result
         return "matrix must be a square matrix"
-
-mat0 = [[]]
-mat1 = [[5]]
-mat2 = [[1, 2], [3, 4]]
-mat3 = [[1, 1], [1, 1]]
-mat4 = [[5, 7, 9], [3, 1, 8], [6, 2, 4]]
-mat5 = []
-mat6 = [[1, 2, 3], [4, 5, 6]]
-
-len(mat1)
-len(mat1[0])
-print(isinstance(mat1[0][0], int))
-
-determinant(mat0)
-determinant(mat1)
-determinant(mat2)
-determinant(mat3)
-
-
-mat4 = [[5, 7, 9], [3, 1, 8], [6, 2, 4]]
-for i in range(0, len(mat4[0])):
-    print(i)
-    mat4[0][i] * ()
-    for j in range(0, len(mat4[0])):
-        if j == i:
-            pass
-        #add the cofactor matrix
-
-
-print(mat4[0])
