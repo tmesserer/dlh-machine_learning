3d259a74ed2c6caa1adbfea25b88deece95dac46
Thomas <tc.messerer@gmail.com>
2026-06-23 17:59:23 +0200

    28, 32: adjusted files

--- pipeline/databases/32-update_topics.py
+++ pipeline/databases/32-update_topics.py
@@ -4,5 +4,5 @@
 
 def update_topics(mongo_collection, name, topics):
     """changes all topics of a school document based on the name"""
-    mongo_collection.update_one({"name": name},
+    mongo_collection.update_many({"name": name},
     {"$set": {"topics": topics}})
