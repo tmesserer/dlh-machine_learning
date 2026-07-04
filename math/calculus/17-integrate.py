b98ae600169adcc87b9a4f00966148d1d6d24a78
Thomas <tc.messerer@gmail.com>
2026-07-04 20:15:47 +0200

    17: added file

---
+++ math/calculus/17-integrate.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Module for calculus project"""
+
+
+def poly_integral(poly, C=0):
+    """calculates the integral of a polynomial"""
+    if not isinstance(poly, list) or not all(isinstance(x, int) for x in poly)\
+       or poly == []:
+        return None
+    new_poly = []
+    if isinstance(C, int):
+        new_poly.append(C)
+        new_poly.extend([int(poly[i] / (i+1)) if poly[i] % (i+1) == 0
+                         else poly[i] / (i+1) for i in range(0, len(poly))])
+        return new_poly
+    else:
+        return None
