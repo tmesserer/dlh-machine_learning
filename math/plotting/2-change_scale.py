af8a1d81ef85f610d3b5e0b017c2651a1a92a198
Thomas <tc.messerer@gmail.com>
2026-07-07 20:43:11 +0200

    2: added file

---
+++ math/plotting/2-change_scale.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Module for plotting with matplotlib and pyplot"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def change_scale():
+    x = np.arange(0, 28651, 5730)
+    r = np.log(0.5)
+    t = 5730
+    y = np.exp((r / t) * x)
+    plt.figure(figsize=(6.4, 4.8))
+
+    # your code here
+    plt.xlim(0, 28650)
+    plt.yscale("log")
+    plt.xlabel('Time (years)')
+    plt.ylabel('Fraction Remaining')
+    plt.title('Exponential Decay of C-14')
+    plt.plot(x, y, 'b')
+    plt.show()
