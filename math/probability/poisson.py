bdb9f2be465f1ea4263f4d1f0dd4c021d1d261d6
Thomas <tc.messerer@gmail.com>
2026-07-18 15:19:43 +0200

    0: added file

---
+++ math/probability/poisson.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Module for describing the poisson distribution"""
+import numpy as np
+"""
+π = 3.1415926536
+e = 2.7182818285
+er f(x) = ....
+"""
+
+
+class Poisson:
+    def __init__(self, data=None, lambtha=1.):
+        """initializes the class"""
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(lambtha)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.data = data
+            self.lambtha = float(np.mean(data))
