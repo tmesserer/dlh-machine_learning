fb4c4d83e44d50c4026dda246ba3f809624b7127
Thomas <tc.messerer@gmail.com>
2026-07-18 16:50:55 +0200

    3: added files

---
+++ math/probability/exponential.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""Module for describing the poisson distribution"""
+"""
+π = 3.1415926536
+e = 2.7182818285
+er f(x) = ....
+"""
+
+
+class Exponential:
+    """
+    defines the class Exponential for exp. probabilities
+    """
+    def __init__(self, data=None, lambtha=1.):
+        """initiatilizes an instance under Exponential
+        Args:
+            data: validity checks, list of data
+            lambtha: validity check, calculates it based on data if not given
+        """
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(lambtha)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.data = data
+            self.lambtha = 1/(sum(data)/len(data))
