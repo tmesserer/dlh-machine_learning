7070178c8715cb9b9800a907b43bb97d73eb02f1
Thomas <tc.messerer@gmail.com>
2026-08-01 21:18:21 +0200

    1: added files

---
+++ math/bayesian_prob/1-intersection.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""module for bayesian probability calculations"""
+import numpy as np
+
+
+def intersection(x, n, P, Pr):
+    """
+    function that calculates the likelihood of obtaining this data given
+    hypothetical     probabilities of developing severe side effects
+    Args:
+        x: number of patients that develop severe side effects (binomial dist)
+        n: total number of patients observed
+        P: 1D numpy.ndarray containing the various hypothetical probabilities
+           of developing severe side effects
+        Pr: 1d numpy.ndarray, prior beliefs of P
+    """
+    # Exceptions:
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError("n must be a positive integer")
+    if not isinstance(x, int) or x < 0:
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0")
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+    if not isinstance(P, np.ndarray) or np.ndim(P) != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+    if not isinstance(Pr, np.ndarray) or np.shape(P) != np.shape(Pr):
+        raise TypeError(
+            "Pr must be a numpy.ndarray with the same shape as P")
+    if any(x < 0 or x > 1 for x in P):
+        raise ValueError("All values in P must be in the range [0, 1]")
+    if any(y < 0 or y > 1 for y in Pr):
+        raise ValueError("All values in Pr must be in the range [0, 1]")
+    if not np.isclose(1, np.sum(Pr), rtol=0.00001, atol=0.00001):
+        raise ValueError("Pr must sum to 1")
+
+    # Calculation
+    likelihood = __import__('0-likelihood').likelihood
+    return np.multiply(likelihood(x, n, P), Pr)
