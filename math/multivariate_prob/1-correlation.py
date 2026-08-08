cf175c6d1534e4bd953ce927394e0afa55109d9c
Thomas <tc.messerer@gmail.com>
2026-08-08 18:54:58 +0200

    1: readability adjustments

--- math/multivariate_prob/1-correlation.py
+++ math/multivariate_prob/1-correlation.py
@@ -6,15 +6,18 @@ import numpy as np
 def correlation(C):
     """function that calculates a correlation matrix.
     Args: C: a numpy.ndarray of shape (d, d) containing
-    a covariance matrix"""
+        a covariance matrix
+    Returns: a numpy.ndarray of shape (d, d) with the
+        correlation matrix"""
     if not isinstance(C, np.ndarray):
         raise TypeError("C must be a numpy.ndarray")
     if C.ndim != 2 or C.shape[0] != C.shape[1]:
         raise ValueError("C must be a 2D square matrix")
     # Calculations
+    d = C.shape[0]
     variances = np.diag(C)  # gives a (d, ) array of the diagonal
     std = np.sqrt(variances)
-    std_row = np.reshape(std, (1, len(std)))
-    std_col = np.reshape(std, (len(std), 1))
+    std_row = std.reshape((1, d))  # alternative is np.reshape (std, shape)
+    std_col = std.reshape((d, 1))
     denom_matrix = std_col * std_row
     return C / denom_matrix
