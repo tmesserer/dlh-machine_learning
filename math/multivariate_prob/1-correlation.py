6984a4036563594f9b3d996eb8449237b24ade03
Thomas <tc.messerer@gmail.com>
2026-08-08 18:48:47 +0200

    1: adjusted error check

--- math/multivariate_prob/1-correlation.py
+++ math/multivariate_prob/1-correlation.py
@@ -9,7 +9,7 @@ def correlation(C):
     a covariance matrix"""
     if not isinstance(C, np.ndarray):
         raise TypeError("C must be a numpy.ndarray")
-    if C.shape[0] != C.shape[1]:
+    if C.ndim != 2 or C.shape[0] != C.shape[1]:
         raise ValueError("C must be a 2D square matrix")
     # Calculations
     variances = np.diag(C)  # gives a (d, ) array of the diagonal
