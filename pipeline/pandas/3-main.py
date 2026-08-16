300f8a28dd9c4c49ae52be202f761865b3e7e28d
Thomas <tc.messerer@gmail.com>
2026-08-16 16:55:19 +0200

    added .gitignore and new files for 3

--- pipeline/pandas/3-main.py
+++ pipeline/pandas/3-main.py
@@ -3,7 +3,7 @@
 from_file = __import__('2-from_file').from_file
 rename = __import__('3-rename').rename
 
-df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
+df = from_file('coinbase.csv', ',')
 
 df = rename(df)
 
--- pipeline/pandas/3-rename.py
+++ pipeline/pandas/3-rename.py
@@ -6,5 +6,9 @@ import pandas as pd
 def rename(df):
     """function that takes a pd.DataFrame as input and modifies it"""
     df.rename(columns={"Timestamp": "Datetime"}, inplace=True)
-    df["Datetime"] = pd.to_datetime(df["Datetime"])
+    df["Datetime"] = pd.to_datetime(df["Datetime"], unit='s')
     return df[["Datetime", "Close"]]
+
+# Checking the file and format:
+# new_frame = pd.read_csv("bitstamp.csv", sep=",")
+# print(new_frame.head)
