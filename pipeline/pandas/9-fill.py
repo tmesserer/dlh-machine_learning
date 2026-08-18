061f945a16b7bbea88291161d3d8c195a7b26383
Thomas <tc.messerer@gmail.com>
2026-08-18 17:43:42 +0200

    9: added files

---
+++ pipeline/pandas/9-fill.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Module for using the pandas module in Python"""
+
+
+def fill(df):
+    """function that takes a pd.DataFrame as input and modifies it"""
+    df.drop("Weighted_Price", axis=1, inplace=True)  # remove column
+    # fill with previous row:
+    df[["Close"]] = df[["Close"]].ffill(axis=0, inplace=True)
+
+    # fill with content of "Close"
+    cols_to_fill = ["High", "Low", "Open"]
+    for col in cols_to_fill:
+        df[col] = df[col].fillna(df["Close"], inplace=True)
+
+    # Sets missing values in Volume_(BTC) and Volume_(Currency) to 0
+    df[["Volume_(BTC)", "Volume_(Currency)"]] = \
+        df[["Volume_(BTC)", "Volume_(Currency)"]].fillna(0,
+                                                         axis=0, inplace=True)
+    return df
+
+
+# new_frame = pd.read_csv("bitstamp.csv", sep=",")
+# print(new_frame.head)
