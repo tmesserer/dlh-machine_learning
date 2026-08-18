f3e2d85440238e81fcf456711a0dc94576261060
Thomas <tc.messerer@gmail.com>
2026-08-18 18:02:14 +0200

    9: removed inplace=True where assignment happens

--- pipeline/pandas/9-fill.py
+++ pipeline/pandas/9-fill.py
@@ -6,17 +6,17 @@ def fill(df):
     """function that takes a pd.DataFrame as input and modifies it"""
     df.drop("Weighted_Price", axis=1, inplace=True)  # remove column
     # fill with previous row:
-    df[["Close"]] = df[["Close"]].ffill(axis=0, inplace=True)
+    df[["Close"]] = df[["Close"]].ffill(axis=0)
 
     # fill with content of "Close"
     cols_to_fill = ["High", "Low", "Open"]
     for col in cols_to_fill:
-        df[col] = df[col].fillna(df["Close"], inplace=True)
+        df[col] = df[col].fillna(df["Close"])
 
     # Sets missing values in Volume_(BTC) and Volume_(Currency) to 0
     df[["Volume_(BTC)", "Volume_(Currency)"]] = \
         df[["Volume_(BTC)", "Volume_(Currency)"]].fillna(0,
-                                                         axis=0, inplace=True)
+                                                         axis=0)
     return df
 
 
