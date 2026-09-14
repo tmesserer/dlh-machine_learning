aadafe3a8e050141acbe6cd77cd901bff22dfdcb
Thomas <tc.messerer@gmail.com>
2026-09-14 20:31:01 +0200

    4: added files

--- unsupervised_learning/clustering/4-main.py
+++ unsupervised_learning/clustering/4-main.py
@@ -2,25 +2,18 @@
 #!/usr/bin/env python3
 
 import numpy as np
-import matplotlib.pyplot as plt
-optimum_k = __import__('3-optimum').optimum_k
+initialize = __import__('4-initialize').initialize
 
-if __name__ == "__main__":
-    np.random.seed(0)
-    a = np.random.multivariate_normal([30, 40], [[16, 0], [0, 16]], size=50)
-    b = np.random.multivariate_normal([10, 25], [[16, 0], [0, 16]], size=50)
-    c = np.random.multivariate_normal([40, 20], [[16, 0], [0, 16]], size=50)
-    d = np.random.multivariate_normal([60, 30], [[16, 0], [0, 16]], size=50)
-    e = np.random.multivariate_normal([20, 70], [[16, 0], [0, 16]], size=50)
-    X = np.concatenate((a, b, c, d, e), axis=0)
+if __name__ == '__main__':
+    np.random.seed(11)
+    a = np.random.multivariate_normal([30, 40], [[75, 5], [5, 75]], size=10000)
+    b = np.random.multivariate_normal([5, 25], [[16, 10], [10, 16]], size=750)
+    c = np.random.multivariate_normal([60, 30], [[16, 0], [0, 16]], size=750)
+    d = np.random.multivariate_normal([20, 70], [[35, 10], [10, 35]], size=1000)
+    X = np.concatenate((a, b, c, d), axis=0)
     np.random.shuffle(X)
-
-    results, d_vars = optimum_k(X, kmax=10)
-    print(results)
-    print(np.round(d_vars, 5))
-    plt.scatter(list(range(1, 11)), d_vars)
-    plt.xlabel('Clusters')
-    plt.ylabel('Delta Variance')
-    plt.title('Optimizing K-means')
-    plt.show()
+    pi, m, S = initialize(X, 4)
+    print(pi)
+    print(m)
+    print(S)
 # %%
